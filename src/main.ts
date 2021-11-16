d8767f60724762e7ac0c0f857c602b073eab74a4
Alex Burner <alex.burner@gmail.com>
2021-11-15 21:32:13 -0800

    Install d3-force

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,3 @@ import './style.css'
 const app = document.querySelector<HTMLDivElement>('#app')
 
 if (!app) throw new Error('Missing #app element')
-
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
